46133df1b5e33f2dbf96cb184d9e992284e89fee
kimsj00 <tmdwo8814@gmail.com>
2024-01-21 18:48:58 +0900

    CREATE 프린터큐.py

--- 3주차/프린터큐.py
+++ 3주차/프린터큐.py
@@ -1,2 +1,60 @@
 # 백준 1966 - 프린터 큐
+from collections import deque
+
+T = int(input())
+
+def printer(T):
+    answer = []
+
+    for _ in range(T):
+        N, M = map(int, input().split())
+        deq_count = deque()
+        deq_imp = deque()
+        count = 1
+        
+        lst = list(map(int, input().split()))
+
+        for i, j in enumerate(lst):
+            deq_imp.append(j)
+            deq_count.append(i)
+
+        while True:
+            check = False
+            for i in range(0, len(deq_imp)):
+                if deq_imp[0] < deq_imp[i]:
+                    check = True
+                    break
+            
+            if check :
+                deq_imp.append(deq_imp.popleft())
+                deq_count.append(deq_count.popleft())
+            else:
+                if deq_count[0] == M:
+                    answer.append(count)
+                    break
+                else:
+                    deq_imp.popleft()
+                    deq_count.popleft()
+                    count += 1
+    
+    return answer
+
+for i in printer(T):
+    print(i)
+
+
+                    
+
+
+        
+        
+
+        
+
+    
+
+
+
+
+
 
